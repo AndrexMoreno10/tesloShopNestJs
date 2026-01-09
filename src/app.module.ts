51da1e415fc4f00245b1afee04dad5304abfabde
AndrexMoreno10 <adp2333@gmail.com>
2026-01-09 13:24:56 -0500

    update app.module with serve-static and and other modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ 
+  imports: [
     ConfigModule.forRoot(),
 
     TypeOrmModule.forRoot({
@@ -17,12 +21,20 @@ import { CommonModule } from './common/common.module';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true, //Automatically load entities
       synchronize: true //Note: set to false in production
-}),
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
 
     ProductsModule,
 
-    CommonModule],
+    CommonModule,
+
+    SeedModule,
+
+    FilesModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
